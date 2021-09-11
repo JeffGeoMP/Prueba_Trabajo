8047700161317373e17d13ca7777042d045519c9
Jefferson Geovanny <jeffgeomp@gmail.com>
2021-09-11 00:00:39 -0600

    add: Estructura Inicial Servidor

---
+++ server/src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+
+/**
+ * Configuracion del servidor
+ */
+ 
+const app = express();
+const port = 3000;
+
+app.set('port', port);
+
+
+/**
+ * Miiddlewares
+ */
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+/**
+ * Rutas del Servidor
+ */
+//app.use(require('./routes/routes'));
+
+
+/**
+ * Ejecucion del Servidor
+ */
+app.listen(app.get('port'), ()=>{
+    console.log(`API Corriendo en http://localhost:${port}`);
+});
