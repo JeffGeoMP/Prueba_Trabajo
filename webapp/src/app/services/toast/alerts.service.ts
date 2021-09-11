5baefddd8f43af967ded36c6171e775d1ae8a57f
Jefferson Geovanny <jeffgeomp@gmail.com>
2021-09-11 01:38:25 -0600

    add: Alerts Informativos

--- webapp/src/app/services/toast/alerts.service.ts
+++ webapp/src/app/services/toast/alerts.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertsService {
 
-  constructor() { }
+  constructor(private toast: ToastrService) { }
+
+  ShowSucces(texto:string, titulo:string){
+    this.toast.success(texto, titulo);
+  }
+
+  ShowError(texto:string, titulo:string){
+    this.toast.error(texto, titulo);
+  }
+
+  ShowWarning(texto:string, titulo:string){
+    this.toast.warning(texto, titulo);
+  }
+
 }
